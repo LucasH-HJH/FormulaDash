0742ba66ad94d556a14b249521c08a7bd4f4ff75
Lucas H. <defalt1765@gmail.com>
2024-03-04 01:02:29 +0000

    Added Circuit Overview.

--- pages/0_Session Viewer.py
+++ pages/0_Session Viewer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 import time
@@ -38,10 +39,10 @@ def getSessionTimings(event):
 def getSessionDetails(year,event,session):
     sessionDetails = fastf1.get_session(year, event, session)
     sessionDetails.load()
-            
-    return sessionDetails.results.loc[:, ['ClassifiedPosition','HeadshotUrl','FullName','Abbreviation','DriverNumber','TeamName','GridPosition','Q1','Q2','Q3','Time','Status','Points']]
+    return sessionDetails
 
 def displaySessionDetails(sessionDetails, sessionName):
+    sessionDetails = sessionDetails.results.loc[:, ['ClassifiedPosition','HeadshotUrl','FullName','Abbreviation','DriverNumber','TeamName','GridPosition','Q1','Q2','Q3','Time','Status','Points']]
     df = pd.DataFrame(sessionDetails)
     df = df.reset_index(drop=True)
 
@@ -102,6 +103,66 @@ def displaySessionDetails(sessionDetails, sessionName):
     )
     
     return df
+
+def displayCircuitInfo(sessionDetails):
+    lap = sessionDetails.laps.pick_fastest()
+    pos = lap.get_pos_data()
+    circuit_info = sessionDetails.get_circuit_info()
+
+    def rotate(xy, *, angle):
+        rot_mat = np.array([[np.cos(angle), np.sin(angle)],[-np.sin(angle), np.cos(angle)]])
+        return np.matmul(xy, rot_mat)
+
+    # Get an array of shape [n, 2] where n is the number of points and the second
+    # axis is x and y.
+    track = pos.loc[:, ('X', 'Y')].to_numpy()
+
+    # Convert the rotation angle from degrees to radian.
+    track_angle = circuit_info.rotation / 180 * np.pi
+
+    # Rotate and plot the track map.
+    rotated_track = rotate(track, angle=track_angle)
+    plt.plot(rotated_track[:, 0], rotated_track[:, 1])
+
+    offset_vector = [500, 0]  # offset length is chosen arbitrarily to 'look good'
+
+    # Iterate over all corners.
+    for _, corner in circuit_info.corners.iterrows():
+        # Create a string from corner number and letter
+        txt = f"{corner['Number']}{corner['Letter']}"
+
+        # Convert the angle from degrees to radian.
+        offset_angle = corner['Angle'] / 180 * np.pi
+
+        # Rotate the offset vector so that it points sideways from the track.
+        offset_x, offset_y = rotate(offset_vector, angle=offset_angle)
+
+        # Add the offset to the position of the corner
+        text_x = corner['X'] + offset_x
+        text_y = corner['Y'] + offset_y
+
+        # Rotate the text position equivalently to the rest of the track map
+        text_x, text_y = rotate([text_x, text_y], angle=track_angle)
+
+        # Rotate the center of the corner equivalently to the rest of the track map
+        track_x, track_y = rotate([corner['X'], corner['Y']], angle=track_angle)
+
+        # Draw a circle next to the track.
+        plt.scatter(text_x, text_y, color='grey', s=140)
+
+        # Draw a line from the track to this circle.
+        plt.plot([track_x, text_x], [track_y, text_y], color='grey')
+
+        # Finally, print the corner number inside the circle.
+        plt.text(text_x, text_y, txt,
+                va='center_baseline', ha='center', size='small', color='white')
+
+    plt.title(sessionDetails.event['Location'])
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis('equal')
+    st.pyplot(plt)
+
 
 def run():
     col1, col2, col3 = st.columns(3)
@@ -190,8 +251,13 @@ def run():
                     st.info('Practice sessions do not include times.', icon="ℹ️")
                 elif selectedSession in ["Race","Sprint"]:
                     st.info('Times after the first row is the gap from the session leader.', icon="ℹ️")
-                
+
+                #Session Results
                 df = displaySessionDetails(sessionDetails, selectedSession)
+                st.divider()
+                st.subheader("Circuit Overview")
+                #Circuit Info
+                displayCircuitInfo(sessionDetails)
     
     except KeyError as error:
         print("An exception occurred:", error)
